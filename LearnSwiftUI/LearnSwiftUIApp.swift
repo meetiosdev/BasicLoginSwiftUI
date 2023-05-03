a7111e8456af82eec2fcb76ab913029b8531318f
swarajsinghdev <swarajmeet.crebos@gmail.com>
2023-05-03 12:48:42 +0530

    Initial Commit

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by Swarajmeet Singh on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
